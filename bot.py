7b6c14e8ea322bffdb3d24d1d5e6a46dec10f581
seriouslyunderated <43138380+seriouslyunderated@users.noreply.github.com>
2018-09-10 19:49:43 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,13 @@
-
+public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+{
+    if (activity.GetActivityType() == ActivityTypes.Message)
+    {
+        await Conversation.SendAsync(activity, () => new Dialogs.RootDialog());
+    }
+    else
+    {
+        HandleSystemMessage(activity);
+    }
+    var response = Request.CreateResponse(HttpStatusCode.OK);
+    return response;
+}
